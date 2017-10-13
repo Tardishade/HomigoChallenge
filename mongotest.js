09bac71cc6386234a8846997316e027b31fc1c42
Anirudh Iyer <lopaneyo@gmail.com>
2017-10-13 04:46:31 -0400

    lots of massive changes

--- mongotest.js
+++ mongotest.js
@@ -3,67 +3,86 @@ var assert = require('assert');
 
 var url = 'mongodb://localhost:27017/homigoChallenge';
 
-MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected successfully to server");
+// MongoClient.connect(url, function(err, db) {
+//     assert.equal(null, err);
+//     console.log("Connected successfully to server");
 
-    insertDocuments(db, function() {
-        updateDocument(db, function() {});
-        findDocuments(db, function() {
-            db.close;
-        });
-    });
-});
+//     insertDocuments(db, function() {
+//         updateDocument(db, function() {});
+//         findDocuments(db, function() {
+//             db.close;
+//         });
+//     });
+// });
 
-var insertDocument = function(db, collection, document) {
+exports.insertDocument = function(db, collection, document) {
   var myCollection = db.collection(collection);
   myCollection.insert(document);
 };
 
-var findDocuments = function(db, callback) {
+exports.findDocuments = function(db, collection) {
     // Get the documents collection
-    var collection = db.collection('documents');
+    var collection = db.collection(collection);
     // Find some documents
-    collection.find({}).toArray(function(err, docs) {
-      assert.equal(err, null);
-      console.log("Found the following records");
-      console.log(docs)
-      callback(docs);
-    });
-}
-
-var updateDocument = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('documents');
-  // Update document where a is 2, set b equal to 1
-  collection.updateOne({ a : 2 }
-    , { $set: { b : 1 } }, function(err, result) {
-    assert.equal(err, null);
-    assert.equal(1, result.result.n);
-    console.log("Updated the document with the field a equal to 2");
-    callback(result);
-    });  
-}
-
-var removeDocument = function(db, callback) {
-    // Get the documents collection
-    var collection = db.collection('documents');
-    // Delete document where a is 3
-    collection.deleteOne({ a : 3 }, function(err, result) {
-      assert.equal(err, null);
-      assert.equal(1, result.result.n);
-      console.log("Removed the document with the field a equal to 3");
-      callback(result);
-    });    
-}
-
-var indexCollection = function(db, callback) {
-    db.collection('documents').createIndex(
-      { "a": 1 },
-        null,
-        function(err, results) {
-          console.log(results);
-          callback();
-      }
-    );
+    return collection.find({}).toArray();
 };
+
+exports.removeDocuments = function(db, callback, collection) {
+    // Get the documents collection
+    var myCollection = db.collection(collection);
+    // Delete all documents
+    myCollection.remove();
+};
+
+
+exports.createSpecialArray = function(collection, data) {
+  var arr = [];
+  if (collection === "Customers") {
+    data.forEach((customer) => {
+      var temp = customerParse(customer);
+      arr.push(temp);
+    });
+  } else if (collection === "Bills") {
+    data.forEach((bill) => {
+      var temp = billParse(bill);
+      arr.push(temp);
+    });  
+  } else {
+    data.forEach((invoice) => {
+      var temp = invoiceParse(invoice);
+      arr.push(temp);
+    }); 
+  }
+  return arr;
+}; 
+
+// Being passed an object, returning an array of strings.
+function customerParse(customer) {
+  var arr = []
+  (customer.Id) ? arr.push(customer.Id) : arr.push("N/A");
+  (customer.City) ? arr.push(customer.City) : arr.push("N/A");
+  (customer.Line1) ? arr.push(customer.Line1) : arr.push("N/A");
+  (customer.PostalCode) ? arr.push(customer.PostalCode) : arr.push("N/A");
+  (customer.CompanyName) ? arr.push(customer.CompanyName) : arr.push("N/A");
+  (customer.PrimaryPhone.FreeFormNumber) ? arr.push(customer.PrimaryPhone.FreeFormNumber) : arr.push("N/A");
+  (customer.Balance) ? arr.push(customer.Balance) : arr.push("N/A");
+  return arr;
+}
+
+function billParse(customer) {
+  var arr = []
+  (customer.Id) ? arr.push(customer.Id) : arr.push("N/A");
+  (customer.Balance) ? arr.push(customer.Balance) : arr.push("N/A");
+  (customer.DueDate) ? arr.push(customer.DueDate) : arr.push("N/A");
+  (customer.TxnDate) ? arr.push(customer.TxnDate) : arr.push("N/A");
+  (customer.Line.Desc) ? arr.push(customer.CompanyName) : arr.push("N/A");
+  (customer.PrimaryPhone.FreeFormNumber) ? arr.push(customer.PrimaryPhone.FreeFormNumber) : arr.push("N/A");
+  (customer.Balance) ? arr.push(customer.Balance) : arr.push("N/A");
+  return arr;
+}
+
+function invoiceParse(invoice) {
+  var arr = []
+  //parse
+  return arr;
+}
